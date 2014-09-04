043e8bb51a4d0400ff1435cc6bdb9e93c5ad4a12
slowknight <slowknight3@gmail.com>
2014-09-03 20:18:45 -0700

    Refactored toolbar.js : Swapped factory functions with constructors for toolbar and item.

--- t00lbar.js
+++ t00lbar.js
@@ -1,82 +1,80 @@
 var t00lbar = (function (t00lbar) {
 
-	var createToolbarItem = function (element) {
-		
-		var item = {};
+	var Item = function (element) {
+		Object.defineProperty(this, "element", {
+			value : element
+		});
+	};
 
-		Object.defineProperties(item, {
-			element : {
-				value : element,
-				writable : false
+	Object.defineProperties(Item.prototype, {
+		enabled : {
+			get : function () {
+				return !this.element.classList.contains("disabled");
 			},
-			enabled : {
-				get : function () {
-					return !this.element.classList.contains("disabled");
-				},
-				set : function (value) {
+			set : function (value) {
+				if (value) {
+					this.element.classList.remove("disabled");
+				} else {
+					this.element.classList.add("disabled");
+				}
+			}
+		},
+		activated : {
+			get : function () {
+				return this.element.classList.contains("activated");
+			},
+			set : function (value) {
+				if (this.enabled) {
 					if (value) {
-						this.element.classList.remove("disabled");
+						return this.element.classList.add("activated");
 					} else {
-						this.element.classList.add("disabled");
+						return this.element.classList.remove("activated");
 					}
 				}
-			},
-			activated : {
-				get : function () {
-					return this.element.classList.contains("activated");
-				},
-				set : function (value) {
-					if (this.enabled) {
-						if (value) {
-							return this.element.classList.add("activated");
-						} else {
-							return this.element.classList.remove("activated");
-						}
-					}
-				},
 			}
-		});
-
-		return item;
-	};
+		}
+	});
 
 	var createToolbarItems = function (itemElements) {
 		
 		var items = [];
 
 		Array.prototype.forEach.call(itemElements, function (element, index, array) {
-			var item = createToolbarItem(element);
+			var item = new Item(element);
 			items.push(item);
 		});
 
 		return items;
 	};
 
-	
-	t00lbar.createToolbar = function (elementId) {
-		
-		var element = document.getElementById(elementId);
-		
-		if (!element) {
-			element = document.createElement("div");
-			element.id = elementId;
-			element.className = "toolbar";
-		};
-		
+	var Toolbar = function (element) {
 		var items = element.querySelectorAll(".toolbar-item");
 
-		var toolbar = {
-			add : function () {
+		Object.defineProperties(this, {
+			element : {
+				value : element
+			},
+			items : {
+				value : createToolbarItems(items)
+			}
+		});
+	};
+
+	Object.defineProperties(Toolbar.prototype, {
+		add : {
+			value : function () {
 				var newSpan = document.createElement("span");
 				newSpan.className = "toolbar-item";
 
 				this.element.appendChild(newSpan);
 
-				var item = createToolbarItem(newSpan);
+				var item = new Item(newSpan);
 
 				this.items.push(item);
-			},
-			remove : function (index) {
+			}
+		},
+		remove : {
+			value : function (index) {
 				var len = this.items.length;
 
 				index = (typeof index === 'undefined') ? (len - 1) : index;
@@ -90,22 +88,25 @@ var t00lbar = (function (t00lbar) {
 				this.items.splice(index, 1);
 				
 				this.element.removeChild(item.element);
-			},
-			appendTo : function (parentElement) {
+			}
+		},
+		appendTo : {
+			value : function (parentElement) {
 				parentElement.appendChild(this.element);
 			}
+		}
+	});
+	
+	t00lbar.createToolbar = function (elementId) {		
+		var element = document.getElementById(elementId);
+		
+		if (!element) {
+			element = document.createElement("div");
+			element.id = elementId;
+			element.className = "toolbar";
 		};
 		
-		Object.defineProperties(toolbar, {
-			element : {
-				value : element
-			},
-			items : {
-				value : createToolbarItems(items)
-			}
-		});
-
-		return toolbar;
+		return new Toolbar(element);
 	};
 
 	return t00lbar;
